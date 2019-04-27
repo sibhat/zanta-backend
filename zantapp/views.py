f75b49c762bc89c54e9bb2ab4b5c4d3f8de7d96d
sibhat <sibhat.temelso@gmail.com>
2019-04-26 19:54:29 -0700

    Comment started

--- zantapp/views.py
+++ zantapp/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render
-from rest_framework.decorators import  permission_classes
+# from django.shortcuts import render
+# from rest_framework.decorators import  permission_classes
 
-from oauth2_provider.models import AccessToken
-from oauth2_provider.views import TokenView
-from django.contrib.auth.models import Group
+# from oauth2_provider.models import AccessToken
+# from oauth2_provider.views import TokenView
+# from django.contrib.auth.models import Group
 
 from rest_framework import viewsets
 # Create your views here.
-from zantapp.models import *
+# from zantapp.models import *
 from zantapp.serializers import *
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited. to be deleted!
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
