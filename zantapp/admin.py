94b9840646d550d64bc7bfd6da86b368363b03f6
sibhat <sibhat.temelso@gmail.com>
2019-04-26 19:58:08 -0700

    Admin Comment started

--- zantapp/admin.py
+++ zantapp/admin.py
@@ -4,6 +4,7 @@ from .models import *
 
 # @register(User)
 class UserAdmin(admin.ModelAdmin):
+    """"UserAdmin Comment started"""
     def __init__(self, model, admin_site):
         self.list_display = [field.name for field in model._meta.fields if field.name != "id"]
         super(UserAdmin, self).__init__(model, admin_site)
