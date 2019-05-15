e691884fe301d700fa64381e5f24ec26e36e3023
sibhat <sibhat.temelso@gmail.com>
2019-05-15 10:44:44 -0700

    implement csrf exempt to signUp client url

--- zantapp/urls.py
+++ zantapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from zantapp.views import *
+from django.views.decorators.csrf import csrf_exempt
 
 router = DefaultRouter(trailing_slash=True)
 router.register('client', ClientViewSet)
@@ -14,6 +15,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path("account/client", csrf_exempt(signup_client.as_view()), name="client"),
     # path('o/token/', views.ProfileTokenView.as_view(), name="token"),
     # view sets
     path('v1/', include(router.urls)),
