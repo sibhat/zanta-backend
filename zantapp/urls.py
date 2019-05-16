e53f48956f9836561f475d24ac582ed560f03042
sibhat <sibhat.temelso@gmail.com>
2019-05-15 18:30:08 -0700

    invitation and /me url added

--- zantapp/urls.py
+++ zantapp/urls.py
@@ -8,15 +8,14 @@ from django.views.decorators.csrf import csrf_exempt
 router = DefaultRouter(trailing_slash=True)
 router.register('client', ClientViewSet)
 router.register("users", UserViewSet)
-router.register('groups', GroupViewSet)
+router.register("invitation", InvitationViewSet)
 
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path("account/client", csrf_exempt(signup_client.as_view()), name="client"),
-    # path('o/token/', views.ProfileTokenView.as_view(), name="token"),
-    # view sets
-    path('v1/', include(router.urls)),
+    path("register/client", csrf_exempt(signup_client), name="client"),
+    path('me/', me, name='me'),
+    path('', include(router.urls)),
 ]
